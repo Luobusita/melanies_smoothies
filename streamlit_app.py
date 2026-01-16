0c0929960c9e09a89e31ff6d1cdb7dd6a0848d42
Luobusita <zhaoming.luo@gmail.com>
2026-01-16 22:31:45 +0800

    Update streamlit_app.py
    padded pd_df

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,12 @@ session = cnx.session()
 #my_dataframe = session.table("smoothies.public.fruit_options")
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
 #显示表的内容在屏幕上
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+#Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
