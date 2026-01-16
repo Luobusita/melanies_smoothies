248bd380c3ba356a266224b2318dedce29931bd0
Luobusita <zhaoming.luo@gmail.com>
2026-01-16 21:04:56 +0800

    Update streamlit_app.py
    Add nutrion infor to fruit_chosen block.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 ##from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -37,8 +38,10 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-    #st.write(ingredients_string)
-    
+        #st.write(ingredients_string)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+      
 ####Python标准要求是用4个空格作为一级缩进，tab键通用性不好
 ## for fruit_chosen in ingredients_list:
 ##which actually means...
@@ -58,8 +61,5 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered,'+name_on_order+'!', icon="✅")
 
-##new section to display smoothiefroot nutrion information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
+
